7c92268924a16770f31f26e64dddc3fbd65fa6fc
cfines <c_fines@u.pacific.edu>
2024-08-26 11:15:10 -0700

    initial commit

---
+++ src/main/java/com/cooksys/team2database/Team2databaseApplication.java
@@ -0,0 +1,13 @@
+package com.cooksys.team2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2databaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2databaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooksys/team2database/Team2databaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.team2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2databaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
