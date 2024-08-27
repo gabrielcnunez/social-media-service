8826527395d0610be3e97150aede82e7cce2cb3a
KennyWorth <kennyworth@gmail.com>
2024-08-27 14:14:47 -0500

    Recommended changes and seeder added.

--- src/main/java/com/cooksys/team2database/entities/Hashtag.java
+++ src/main/java/com/cooksys/team2database/entities/Hashtag.java
@@ -1,7 +1,14 @@
 package com.cooksys.team2database.entities;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,18 +21,21 @@ import lombok.NoArgsConstructor;
 @Data
 public class Hashtag {
 	
-	  @Id
-	  @GeneratedValue
-	  private Long id;
-	  
-	  private String label;
-	  
-	  private Long firstUsed;
-	  
-	  private Long lastUsed;
-	  
-	  @ManyToMany(mappedBy = "hashtagsInTweet")
-	  private List<Tweet> tweetsInHashtag;
+	@Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String label;
+
+    @CreationTimestamp
+    private Timestamp firstUsed;
+
+    @UpdateTimestamp
+    private Timestamp lastUsed;
+
+    @ManyToMany(mappedBy = "hashtags", cascade = CascadeType.ALL)
+    private List<Tweet> tweets;
 
 	  
 }
